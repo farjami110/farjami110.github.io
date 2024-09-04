0bef1ba06009b776d2ab1b9d230104987043fddc
farjami110 <farjami110@gmail.com>
2024-09-04 12:07:17 +0200

    Update

--- Codes/IO_Permission.thy
+++ Codes/IO_Permission.thy
@@ -19,7 +19,7 @@ ax_IOP1 : "antitone \<^bold>\<diamond>\<^sup>1\<^sub>p" and
 ax_IOP2 : "regular_pr \<^bold>\<diamond>\<^sup>2\<^sub>p" and
 ax_IOP3i : "antitone \<^bold>\<diamond>\<^sup>3\<^sub>p" and
 ax_IOP3ii : "\<^bold>\<diamond>\<^sup>3\<^sub>p (\<phi> \<^bold>\<and> \<^bold>\<diamond>\<^sup>3\<^sub>o \<phi>) \<^bold>\<le> (\<^bold>\<diamond>\<^sup>3\<^sub>p \<phi>)" and
-ax_IOP4i : "regular_pr \<^bold>\<diamond>\<^sup>4\<^sub>o" and
+ax_IOP4i : "regular_pr \<^bold>\<diamond>\<^sup>4\<^sub>p" and
 ax_IOP4ii : "\<^bold>\<diamond>\<^sup>4\<^sub>p (\<phi> \<^bold>\<and> \<^bold>\<diamond>\<^sup>4\<^sub>o \<phi>) \<^bold>\<le> (\<^bold>\<diamond>\<^sup>4\<^sub>p \<phi>)"
 
 (*Soundness out1*)
@@ -32,7 +32,8 @@ lemma IO1PAND: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>1\<^sub>p\<alpha
 (*Soundness out2*)
 lemma IO2PTOP: "\<^bold>\<bottom> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<^bold>\<top> " by (simp add: setfalse_def)
 lemma IO2PSI: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<beta>) \<and> (\<alpha> \<^bold>\<le> \<beta>)) \<longrightarrow> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p \<alpha>)" 
-  unfolding regular_pr_def regular_pr_def   oops
+  unfolding regular_pr_def 
+  by (metis ORDERtoOR ax_IOP2 regular_pr_def setand_def)
 lemma IO2PWO: "((\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<alpha>) \<and> (\<phi> \<^bold>\<le> \<psi>)) \<longrightarrow> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<alpha>)"  by simp
 lemma IO2PAND: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<alpha>) \<and> (\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<alpha>)) \<longrightarrow> ((\<phi>\<^bold>\<or>\<psi>)\<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<alpha>)"  by (simp add: setor_def)
 lemma IO2POR: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<alpha>) \<and> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<beta>))  \<longrightarrow> (\<phi> \<^bold>\<le>  \<^bold>\<diamond>\<^sup>2\<^sub>p(\<alpha>\<^bold>\<or>\<beta>) )"  using ax_IOP2  
@@ -41,14 +42,15 @@ lemma IO2POR: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>2\<^sub>p\<alpha>
 (*Soundness out3*)
 lemma IO3PTOP: "\<^bold>\<bottom> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p\<^bold>\<top> " by (simp add: setfalse_def)
 lemma IO3PSI: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p\<beta>) \<and> (\<alpha> \<^bold>\<le> \<beta>)) \<longrightarrow> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p \<alpha>)" using  ax_IOP3i 
-  using antitone_def antitone_def by fastforce
+  using antitone_def by fastforce
 lemma IO3PWO: "((\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p\<alpha>) \<and> (\<phi> \<^bold>\<le> \<psi>)) \<longrightarrow> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p\<alpha>)" by simp
 lemma IO3PAND: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p\<alpha>) \<and> (\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p\<alpha>)) \<longrightarrow> ((\<phi>\<^bold>\<or>\<psi>)\<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p\<alpha>)" by (simp add: setor_def)
 lemma IO3PCT: " ((\<^bold>\<diamond>\<^sup>3\<^sub>o \<alpha> \<^bold>\<le> \<phi>) \<and> (\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>3\<^sub>p (\<alpha>\<^bold>\<and>\<phi>))) \<longrightarrow> (\<psi> \<^bold>\<le>  \<^bold>\<diamond>\<^sup>3\<^sub>p\<alpha>)"  using ax_IOP3i oops
 
 (*Soundness out4*)
 lemma IO4PTOP: "\<^bold>\<bottom> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<^bold>\<top> " by (simp add: setfalse_def)
-lemma IO4PSI: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<beta>) \<and> (\<alpha> \<^bold>\<le> \<beta>)) \<longrightarrow> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p \<alpha>)"   using ax_IOP1  oops
+lemma IO4PSI: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<beta>) \<and> (\<alpha> \<^bold>\<le> \<beta>)) \<longrightarrow> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p \<alpha>)"   using ax_IOP1  
+  by (metis ORDERtoOR ax_IOP4i regular_pr_def setand_def)
 lemma IO4WO: "((\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<alpha>) \<and> (\<phi> \<^bold>\<le> \<psi>)) \<longrightarrow> (\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<alpha>)" by simp
 lemma IO4PAND: "((\<phi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<alpha>) \<and> (\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<alpha>)) \<longrightarrow> ((\<phi>\<^bold>\<or>\<psi>)\<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<alpha>)" by (simp add: setor_def)
 lemma IO4PCT: " ((\<^bold>\<diamond>\<^sup>4\<^sub>o \<alpha> \<^bold>\<le> \<phi>) \<and> (\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p (\<alpha>\<^bold>\<and>\<phi>))) \<longrightarrow> (\<psi> \<^bold>\<le> \<^bold>\<diamond>\<^sup>4\<^sub>p\<alpha>)"  using ax_IOP4i oops
